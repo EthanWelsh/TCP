616f5fdba031066077498694eb589ffd9eae0a46
welshej <welshej@gmail.com>
2014-10-14 01:22:10 -0400

    bash scripts for deployment

---
+++ bash_scripts/connect.bash
@@ -0,0 +1,2 @@
+ssh -X ejw45@netlab-3.cs.pitt.edu
+echo /opt/ejw45/minet/
---
+++ bash_scripts/deploy.bash
@@ -0,0 +1,2 @@
+scp -r minet/src/modules/tcp_module.cc ejw45@netlab-3.cs.pitt.edu:/opt/ejw45/minet/src/modules/tcp_module.cc
+
